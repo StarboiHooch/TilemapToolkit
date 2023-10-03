32ea5b9bba2392dbf6ed5494d059e8e7c7534233
hirokironimo <henrylwilkinson@gmail.com>
2023-10-03 17:47:12 +0100

    changes to customruletile

--- CustomRuleTile.cs
+++ CustomRuleTile.cs
@@ -2,20 +2,20 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 
 [CreateAssetMenu]
-public class CustomRuleTile : RuleTile<CustomRuleTile.Neighbor>
+public class CustomRuleTile : RuleTile<RuleTile.TilingRule.Neighbor>
 {
-    public class Neighbor : RuleTile.TilingRule.Neighbor
-    {
-        public const int Null = 2;
-        public const int NotNull = 1;
-    }
+    //public class Neighbor : RuleTile.TilingRule.Neighbor
+    //{
+    //    public const int NotThis = 2;
+    //    public const int This = 1;
+    //}
 
     public override bool RuleMatch(int neighbor, TileBase tile)
     {
         switch (neighbor)
         {
-            case Neighbor.Null: return tile == null;
-            case Neighbor.NotNull: return tile != null;
+            case RuleTile.TilingRule.Neighbor.NotThis: return tile != this;
+            case RuleTile.TilingRule.Neighbor.This: return tile == this;
         }
         return base.RuleMatch(neighbor, tile);
     }
